4740c75c08e0ba4d6af8a2a3c33662c47edd6d91
Aubin ABADIE <aubin.abadie@gmail.com>
2018-06-01 12:31:54 +0200

    Update cart view

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -15,7 +15,7 @@
   <div class="check">
       <div class="container">
           <div class="col-md-3 cart-total">
-              <a class="continue" href="#">Continue to basket</a>
+              <a class="continue" href="/">Continue to basket</a>
               <div class="price-details">
                   <h3>Price Details</h3>
                   <span>Total</span>
@@ -46,56 +46,28 @@
                       });
                  </script>
               <div class="cart-header">
-      <div class="close1"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></div>
-                  <div class="cart-sec simpleCart_shelfItem">
-                          <div class="cart-item cyc">
-                              <img src="images/grid8.jpg" class="img-responsive" alt=""/>
-                          </div>
-                         <div class="cart-item-info">
-                              <ul class="qty">
-                                  <li><p>Size : 9 US</p></li>
-                                  <li><p>Qty : 1</p></li>
-                                  <li><p>Price each : $190</p></li>
-                              </ul>
-                              <div class="delivery">
-                                   <p>Service Charges : Rs.190.00</p>
-                                   <span>Delivered in 2-3 bussiness days</span>
-                                   <div class="clearfix"></div>
-                              </div>
-                         </div>
-                         <div class="clearfix"></div>
+              <div class="close1"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></div>
 
-                    </div>
-               </div>
-               <script>$(document).ready(function(c) {
-                      $('.close2').on('click', function(c){
-                              $('.cart-header2').fadeOut('slow', function(c){
-                          $('.cart-header2').remove();
-                      });
-                      });
-                      });
-               </script>
-              <div class="cart-header2">
-      <div class="close2"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></div>
-                      <div class="cart-sec simpleCart_shelfItem">
-                          <div class="cart-item cyc">
-                               <img src="images/grid7.jpg" class="img-responsive" alt=""/>
-                          </div>
-                          <div class="cart-item-info">
-                              <ul class="qty">
-                                  <li><p>Size : 8 US</p></li>
-                                  <li><p>Qty : 2</p></li>
-                                  <li><p>Price each : $190</p></li>
-                              </ul>
-                              <div class="delivery">
-                                  <p>Service Charges : Rs.360.00</p>
-                                  <span>Delivered in 2-3 bussiness days</span>
-                                  <div class="clearfix"></div>
-                              </div>
-                         </div>
-                         <div class="clearfix"></div>
+                  <div class="cart-sec simpleCart_shelfItem">
+                      <div class="cart-item cyc">
+                          <img src="images/grid8.jpg" class="img-responsive" alt=""/>
                       </div>
-              </div>
+                     <div class="cart-item-info">
+                          <ul class="qty">
+                              <li><p>Manga: One Piece</p></li>
+                              <li><p>Vol. 1</p></li>
+                              <li><p>Qty: 1</p></li>
+                              <li><p>Unit price: $190</p></li>
+                          </ul>
+                          <div class="delivery">
+                               <p>Service Charges : Rs.190.00</p>
+                               <span>Delivered in 2-3 bussiness days</span>
+                               <div class="clearfix"></div>
+                          </div>
+                     </div>
+                     <div class="clearfix"></div>
+                  </div>
+               </div>
           </div>
           <div class="clearfix"> </div>
       </div>
